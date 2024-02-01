66afc931bfef7c44c0965c83367e319c4a88dc11
Rana Banik <rana.banik@vanderbilt.edu>
2024-02-01 11:40:46 -0600

    differential analysis + XGBoost to be run w msaligned

--- Codes/differentialAnalysis.py
+++ Codes/differentialAnalysis.py
@@ -1,9 +1,10 @@
 # 01.04.2024
+# done after peak alignment
 from glob import glob
 import os
 import h5py
 import matplotlib.pyplot as plt
-import seaborn as sn
+import seaborn as sns
 import matplotlib as mtl
 mtl.use("TKAgg")
 from matplotlib.colors import LinearSegmentedColormap
@@ -11,26 +12,25 @@ from sklearn.decomposition import PCA, KernelPCA, FactorAnalysis
 from sklearn.preprocessing import StandardScaler as SS
 from sklearn.preprocessing import MinMaxScaler
 from Codes.Utilities import find_nearest, _smooth_spectrum, umap_it, hdbscan_it, chart
-from Codes.Utilities import saveimages, poissonScaling
+from Codes.Utilities import saveimages, poissonScaling, kdemzs
 from scipy.signal import argrelextrema
 from scipy.stats import kruskal
 import pandas as pd
-import shap
 import numpy as np
 from pyimzml.ImzMLParser import _bisect_spectrum
-from xgboost import XGBClassifier
 from sklearn.model_selection import KFold
 from sklearn.datasets import load_iris
 from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.inspection import permutation_importance
 rng = np.random.RandomState(31337)
 
-fileDir = r'/media/banikr/DATA/MALDI/230713-Chen-intactproteins/'
+# fileDir = r'/media/banikr/DATA/MALDI/230713-Chen-intactproteins/'
+fileDir = r'C:\Data\MALDI\230713-Chen-intactproteins'
 
 h5Path = os.path.join(fileDir, 'SavGolwl55po2BaseCorrdeg6max1000tol5XlocYlocRegID.h5')
 
 with h5py.File(h5Path, 'r') as pfile:
-    print(pfile.keys())
+    # print(pfile.keys())
     processedSpectra = np.array(pfile['processedspectra'])
     mzs = np.array(pfile['rawmzs'])
     xCor = np.array(pfile['xCor'])
@@ -38,12 +38,19 @@ with h5py.File(h5Path, 'r') as pfile:
     corRegID = np.array(pfile['corRegID'])
 meanSpec = np.mean(processedSpectra, axis=0)
 peakPositions = argrelextrema(meanSpec, np.greater)[0]
-# processedSpectra = processedSpectra[:, peakPositions]
-# print("peak selected spectra:", processedSpectra.shape)
-peakmzs = mzs#[peakPositions]
+peakmzs = mzs[peakPositions]
+processedSpectra = processedSpectra[:, peakPositions]
+print("peak selected spectra:", processedSpectra.shape)
 
-gmcsvDir = r'/media/banikr/DATA/MALDI/230713-Chen-intactproteins/GM_injured_cord1/'
-wmcsvDir = r'/media/banikr/DATA/MALDI/230713-Chen-intactproteins/WM_injured_cord1/'
+h5Path = os.path.join(fileDir, 'alignedSpectra.h5')
+with h5py.File(h5Path, 'r') as pfile:
+    # print(pfile.keys())
+    alignedSpectra = np.array(pfile['spectra'])
+
+# gmcsvDir = r'/media/banikr/DATA/MALDI/230713-Chen-intactproteins/GM_injured_cord1/'
+gmcsvDir = os.path.join(fileDir, 'GM_injured_cord1')
+# wmcsvDir = r'/media/banikr/DATA/MALDI/230713-Chen-intactproteins/WM_injured_cord1/'
+wmcsvDir = os.path.join(fileDir, 'WM_injured_cord1')
 
 gm1csv = glob(os.path.join(gmcsvDir, '*1*spots.csv'))
 gm2csv = glob(os.path.join(gmcsvDir, '*2*spots.csv'))
@@ -86,26 +93,25 @@ print(np.unique(labels), len(labels))
 specIdx = []
 for df in dflist:
     specIdx.extend(df['Spot index'].values)
-
 print(len(specIdx))
-# spectra = processedSpectra[specIdx]
-# ionImageSpectra = np.zeros([len(specIdx), len(peakPositions)])
-# tol = 25
-# for idx, spot in enumerate(specIdx):
-#     ints = processedSpectra[spot]
-#     for jdx, mz_value in enumerate(peakmzs):
-#         min_i, max_i = _bisect_spectrum(mzs, mz_value, tol)
-#         ionImageSpectra[idx, jdx] = sum(ints[min_i:max_i + 1])  # = array3D[xpos, ypos, jdx]
-#     # ionImage3D[xCor[spot], yCor[spot], :] = ionImageSpectra[idx]
-# print("ionImageSpectra.shape: ", ionImageSpectra.shape)
+
+ionImageSpectra = np.zeros([len(specIdx), len(peakPositions)])
+tol = 25
+for idx,_  in enumerate(labels):
+    ints = alignedSpectra[idx]
+    for jdx, mz_value in enumerate(peakmzs):
+        min_i, max_i = _bisect_spectrum(mzs, mz_value, tol)
+        ionImageSpectra[idx, jdx] = sum(ints[min_i:max_i + 1])  # = array3D[xpos, ypos, jdx]
+    # ionImage3D[xCor[spot], yCor[spot], :] = ionImageSpectra[idx]
+print("ionImageSpectra.shape: ", ionImageSpectra.shape)
 img = np.zeros([max(xCor)+1, max(yCor)+1])
 print("img.shape", img.shape)
 if 'colors' not in plt.colormaps():
     # colors = [(0.1, 0.1, 0.1), (0.0, 0, 1.0), (0.0, 0.5, 0.5), (0.0, 1.0, 0.0), (0.5, 0.5, 0.0), (1.0, 0, 0.0)]  # Bk -> bl -> G -> r
-    colors = [(0.05, 0.05, 0.05, 1.0),
-              (0.15, 0.15, 0.75, 1.0),
-              (0.75, 0.15, 0.15, 1.0),
-              (0.15, 0.75, 0.15, 1.0),
+    colors = [(0.05, 0.05, 0.05, 0.0), # background
+              (0.65, 0.65, 0.65, 1.0), # wm
+              (0.15, 0.15, 0.15, 1.0), # gm
+              (0.65, 0.15, 0.15, 1.0), # injury
               # (0.15, 0.15, 0.75, 1.0),
               # (0.95, 0.75, 0.05, 1.0),
               # (0.75, 0.15, 0.15, 1.0), # red
@@ -127,63 +133,64 @@ plt.imshow(img[min(xCor):max(xCor)+1, min(yCor):max(yCor)+1], cmap='colors')
 plt.colorbar()
 plt.show()
 # gm - 2, injured - 3, wm - 1
-gmSpotIdx = []
-wmSpotIdx = []
-injuredSpotIdx = []
-for (idx, l) in zip(specIdx, labels):
+gmSpectra = []
+wmSpectra = []
+injuredSpectra = []
+for idx, l in enumerate(labels):
     if l == 2:
-        gmSpotIdx.append(idx)
+        gmSpectra.append(ionImageSpectra[idx])
     if l == 3:
-        injuredSpotIdx.append(idx)
+        injuredSpectra.append(ionImageSpectra[idx])
     if l == 1:
-        wmSpotIdx.append(idx)
+        wmSpectra.append(ionImageSpectra[idx])
 
-print(len(gmSpotIdx), "\n", gmSpotIdx)
-print(len(injuredSpotIdx), "\n", injuredSpotIdx)
-print(len(wmSpotIdx), "\n", wmSpotIdx)
+# print(len(gmSpotIdx), "\n", gmSpotIdx)
+# print(len(injuredSpotIdx), "\n", injuredSpotIdx)
+# print(len(wmSpotIdx), "\n", wmSpotIdx)
 
-gmSpectra = processedSpectra[gmSpotIdx]
-injuredSpectra = processedSpectra[injuredSpotIdx]
-wmSpectra = processedSpectra[wmSpotIdx]
+# gmSpectra = processedSpectra[gmSpotIdx]
+# injuredSpectra = processedSpectra[injuredSpotIdx]
+# wmSpectra = processedSpectra[wmSpotIdx]
 
-print(gmSpectra.shape, injuredSpectra.shape, wmSpectra.shape)
-print(type(gmSpectra), type(injuredSpectra), type(wmSpectra))
+if __name__ != '__main__':
+    gmSpectra = np.array(gmSpectra)
+    injuredSpectra = np.array(injuredSpectra)
+    wmSpectra = np.array(wmSpectra)
+    print(gmSpectra.shape, injuredSpectra.shape, wmSpectra.shape)
+    # print(type(gmSpectra), type(injuredSpectra), type(wmSpectra))
 
-gmMedian = np.median(gmSpectra, axis=0)
-wmMedian = np.median(wmSpectra, axis=0)
-injuredMedian = np.median(injuredSpectra, axis=0)
+    gmMedian = np.median(gmSpectra, axis=0)
+    wmMedian = np.median(wmSpectra, axis=0)
+    injuredMedian = np.median(injuredSpectra, axis=0)
 
-gmMean = np.mean(gmSpectra, axis=0)
-wmMean = np.mean(wmSpectra, axis=0)
-injuredMean = np.mean(injuredSpectra, axis=0)
+    gmMean = np.mean(gmSpectra, axis=0)
+    wmMean = np.mean(wmSpectra, axis=0)
+    injuredMean = np.mean(injuredSpectra, axis=0)
 
-# plt.vlines(peakmzs, 0, gmMean, label='gm', colors='g',
-#                   linestyles='solid', alpha=0.5)
-# plt.vlines(peakmzs, 0, wmMean, label='wm', colors='k',
-#                   linestyles='solid', alpha=0.5)
-# plt.vlines(peakmzs, 0, injuredMean, label='injured', colors='r',
-#                   linestyles='solid', alpha=0.5)
-# plt.legend(fontsize=20)
-# plt.show()
+    # plt.vlines(peakmzs, 0, gmMean, label='gm', colors='g',
+    #                   linestyles='solid', alpha=0.5)
+    # plt.vlines(peakmzs, 0, wmMean, label='wm', colors='k',
+    #                   linestyles='solid', alpha=0.5)
+    # plt.vlines(peakmzs, 0, injuredMean, label='injured', colors='r',
+    #                   linestyles='solid', alpha=0.5)
+    # plt.legend(fontsize=20)
+    # plt.show()
 
-plt.plot(peakmzs, gmMean, 'g', label='gm',
-                   alpha=0.5)
-plt.plot(peakmzs, wmMean, 'k', label='wm',
-                  alpha=0.5)
-plt.plot(peakmzs, injuredMean, 'r', label='injured',
-                   alpha=0.5)
-plt.legend(fontsize=20)
-plt.show()
+    plt.plot(peakmzs, gmMean, 'tab:gray', label='gm', alpha=0.5)
+    plt.plot(peakmzs, wmMean, 'wheat', label='wm', alpha=0.5)
+    plt.plot(peakmzs, injuredMean, 'tab:red', label='injured', alpha=0.5)
+    plt.legend(fontsize=20)
+    plt.show()
 
-plt.fill_between(peakmzs, 0, gmMedian, label='gm', color='g', alpha=0.5, hatch="//")
-plt.fill_between(peakmzs, 0, wmMedian, label='wm', color='k', alpha=0.5, hatch="-")
-plt.fill_between(peakmzs, 0, injuredMedian, label='injured', color='r', alpha=0.5, hatch="\\")
+    plt.fill_between(peakmzs, 0, gmMedian, label='gm', color='tab:gray', alpha=0.4, hatch=None, edgecolor='k', zorder=1)
+    plt.fill_between(peakmzs, 0, wmMedian, label='wm', color='wheat', alpha=0.4, hatch=None, edgecolor='k', zorder=1)
+    plt.fill_between(peakmzs, 0, injuredMedian, label='injured', color='tab:red', alpha=0.4, hatch=None, edgecolor='k', zorder=-1)
 
-plt.legend(fontsize=12)
-plt.xlabel('Peak MZs', fontsize=14)
-plt.ylabel('Median', fontsize=14)
-plt.title('Area Under the Curve Plot', fontsize=16)
-plt.show()
+    plt.legend(fontsize=12)
+    plt.xlabel('Peak MZs', fontsize=14)
+    plt.ylabel('Median', fontsize=14)
+    plt.title('Change in intensity', fontsize=16)
+    plt.show()
 
 # plt.vlines(peakmzs, 0, injuredMedian-nearMedian, label='injured - healthy', colors='r',
 #                   linestyles='solid', alpha=0.5)
@@ -191,3 +198,7 @@ plt.show()
 # plt.legend(fontsize=20)
 # plt.show()
 
+reg1specIdx = []
+for df in [gm1df, wm1df]:
+    reg1specIdx.extend(df['Spot index'].values)
+print(len(reg1specIdx))
